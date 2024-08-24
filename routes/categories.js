8da5c74567bff9c8d395a99160134ccefa20c8c8
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-08-24 23:59:43 +0100

    Delete catagories

--- routes/categories.js
+++ routes/categories.js
@@ -25,4 +25,17 @@ router.post('/', async (req, res) => {
     res.send(category);
 })
 
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndDelete(req.params.id).then((category) =>{
+        if(category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "category not found!"})
+        }
+    }).catch((err)=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+
 module.exports = router;
