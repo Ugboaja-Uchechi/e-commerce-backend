b62c6a6f2ed650460815001c669b0f7af313296a
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-08-25 16:40:59 +0100

    Delete products

--- routes/product.js
+++ routes/product.js
@@ -79,4 +79,16 @@ router.put('/:id',async (req, res)=> {
   res.send(product);
 })
 
+router.delete('/:id', (req, res)=>{
+  Product.findByIdAndDelete(req.params.id).then(product =>{
+      if(product) {
+          return res.status(200).json({success: true, message: 'the product is deleted!'})
+      } else {
+          return res.status(404).json({success: false , message: "product not found!"})
+      }
+  }).catch(err=>{
+     return res.status(500).json({success: false, error: err}) 
+  })
+})
+
 module.exports = router;
