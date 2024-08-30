8b7b37a232db34fe0c66b32bf090a5e1d9566fd9
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-08-30 23:59:04 +0100

    Get amount of total sales

--- routes/orders.js
+++ routes/orders.js
@@ -97,5 +97,17 @@ router.delete('/:id', (req, res)=>{
     })
 })
 
+router.get('/get/totalsales', async (req, res)=> {
+    const totalSales= await Order.aggregate([
+        { $group: { _id: null , totalsales : { $sum : '$totalPrice'}}}
+    ])
+
+    if(!totalSales) {
+        return res.status(400).send('The order sales cannot be generated')
+    }
+
+    res.send({totalsalesMoney: totalSales.pop().totalsales})
+})
+
 
 module.exports =router;
